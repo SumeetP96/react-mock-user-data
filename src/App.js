74126e29298cdb56bdb1b10923c2c80593359f22
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2023-01-26 15:09:01 +0530

    Clear preset ui and integrate bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header py-2 bg-light d-flex justify-content-center align-items-center">
+        <img src={logo} height={70} className="App-logo" alt="logo" />
+        <h1>User Data</h1>
       </header>
     </div>
   );
